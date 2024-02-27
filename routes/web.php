3567162f5c3ed91c364cc3c3519a4d4c61790eb5
iqballmhtrm <iqbalmuctar@gmail.com>
2024-02-27 09:08:46 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -25,53 +25,53 @@ use App\Http\Controllers\WelcomeController;
 |
 */
 
-// Route::get('/welcome', function () {
-//     return view('welcome');
-// });
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
-// Route::get('/hello', function () {
-//     return 'Hello World';
-// });
+Route::get('/hello', function () {
+    return 'Hello World';
+});
 
-// Route::get('/world', function () {
-//     return 'World';
-// });
+Route::get('/world', function () {
+    return 'World';
+});
 
-// Route::get('/', function () {
-//     return 'selamat datang';
-// });
+Route::get('/', function () {
+    return 'selamat datang';
+});
 
-// Route::get('/about', function () {
-//     return '2241720265';
-// });
-
-
-// Route::get('/user/{name}', function ($name) {
-//     return 'Nama saya ' . $name;
-// });
+Route::get('/about', function () {
+    return '2241720265';
+});
 
 
-// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-//     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
-// });
-
-// Route::get('/articles/{id}', function ($id) {
-//     return 'halaman artike dengan id ' . $id;
-// });
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
 
 
-// Route::get('/user/{name?}', function ($name=null) {
-//     return 'Nama saya '.$name;
-// });
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
 
-// Route::get('/user/{name?}', function ($name = 'John') {
-//     return 'Nama saya ' . $name;
-// });
+Route::get('/articles/{id}', function ($id) {
+    return 'halaman artike dengan id ' . $id;
+});
 
 
-// Route::get('/user/profile', function () {
-//     //
-// })->name('profile');
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
 
 
 Route::middleware(['first', 'second'])->group(function () {
